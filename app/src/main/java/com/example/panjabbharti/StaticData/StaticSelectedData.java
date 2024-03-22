fb9a984d015c4bafd2c676470153fc8522aa0280
MANJINDER SINGH <manjindersingh1212004@gmail.com>
2024-03-22 19:25:58 +0530

    basic structure for filter is build for qualification field with the help of recycler view and adapters which deals with dummy data

---
+++ app/src/main/java/com/example/panjabbharti/StaticData/StaticSelectedData.java
@@ -0,0 +1,11 @@
+package com.example.panjabbharti.StaticData;
+
+import java.util.ArrayList;
+
+public class StaticSelectedData {
+    public static ArrayList<String> selectedQualification;
+
+    public static void resetData(){
+        selectedQualification=new ArrayList<>();
+    }
+}
